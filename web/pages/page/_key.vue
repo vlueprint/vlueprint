09193aea5b2175e3f3eaabd133faa8d666909959
Appachan <appachan.hotel@gmail.com>
2020-03-24 00:13:43 +0900

    add routing to resource/schema/page

---
+++ web/pages/page/_key.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>{{ key }}</h1>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  async asyncData ({ params }) {
+    return {
+      key: params.key
+    }
+  }
+})
+</script>
---
+++ web/pages/resource/_key.vue
@@ -0,0 +1,8 @@
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  fetch({ redirect, params }) {
+    redirect(303, `/page/${params.key}`)
+  }
+})
+</script>

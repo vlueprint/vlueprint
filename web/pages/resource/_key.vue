13c15205a1a03ccf2b3d990f8aa77d72594dfb67
Appachan <appachan.hotel@gmail.com>
2020-03-28 15:32:42 +0900

    avoid redirecting to `/resource/undefined`

--- web/pages/resource/_key.vue
+++ web/pages/resource/_key.vue
@@ -2,7 +2,7 @@
 import Vue from 'vue'
 export default Vue.extend({
   fetch({ redirect, params }) {
-    redirect(303, `/page/${params.key}`)
+    redirect(303, `/page/${params.key || ''}`)
   }
 })
 </script>

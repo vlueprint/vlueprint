98a67195ad00ffb45fd0d4cc39ed381285800298
Appachan <appachan.hotel@gmail.com>
2020-03-28 19:05:40 +0900

    add web document for resource

--- web/pages/_namespace/_key.vue
+++ web/pages/_namespace/_key.vue
@@ -1,12 +1,66 @@
 <template>
-  <h1>{{ key }}</h1>
+  <section class="main-content section">
+    <div class="container">
+      <h1 class="title is-1">
+        About: {{ label }}
+      </h1>
+      <sparql-response-table :response="response" />
+    </div>
+  </section>
 </template>
+
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({
-  async asyncData ({ params }) {
+import SparqlResponseTable from '~/components/SparqlResponseTable.vue'
+
+import { SparqleResponse } from '~/types/SparqleResponse.d.ts'
+
+interface Computed {
+  label: string;
+}
+
+export default Vue.extend<any, any, Computed, any>({
+  components: { SparqlResponseTable },
+  head () {
     return {
-      key: params.key
+      title: `${this.label} - vlueprint`,
+      //title: ` - vlueprint`,
+      meta: [
+        { hid: 'description', name: 'description', content: `vlueprint の「${this.label}」のページです。` }
+      ]
+    }
+  },
+  computed: {
+    label (): string {
+      let ret = ''
+      // @ts-ignore
+      this.response.results.bindings.forEach((binding) => {
+        const labelUri = 'http://www.w3.org/2000/01/rdf-schema#label'
+        if (binding.Property.value === labelUri) { ret = binding.Value.value }
+      })
+      return ret
+    }
+  },
+  async asyncData ({ $axios, params, error }) {
+    const baseUrl = 'https://vlueprint.org'
+    const namespace = (params.namespace === 'page') ? 'resource' : params.namespace
+    const subjectUrl = `${baseUrl}/${namespace}/${params.key}`
+    const query = `SELECT ?Property ?Value WHERE { <${subjectUrl}> ?Property ?Value }`
+    try {
+      const response = await $axios.get<SparqleResponse>('/sparql', {
+        params: { query },
+      })
+      if (response.data.results.bindings.length) {
+        return {
+          response: response.data,
+          key: params.key,
+          className: params.class
+        }
+      } else {
+        error({ statusCode: 404, message: 'Data not found' })
+      }
+    } catch (e) {
+      error({ statusCode: 404, message: 'Data not found' })
     }
   }
 })

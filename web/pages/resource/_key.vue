42a54c0aa43504f71d3e30f83d4c575f18eb218e
Appachan <appachan.hotel@gmail.com>
2020-03-28 23:48:38 +0900

    URI に紐づくWebページが欲しい #30 (#32)
    * add routing to resource/schema/page
    
    * avoid redirecting to `/resource/undefined`
    
    * add URL encoding for non-ASCII characters
    
    * add SPARQL query response view component
    
    * merge routing to page/ & schema/
    
    * tweak
    
    * add web document for resource
    
    * fix resource-anchors href
    
    * tweak
    
    * tweak: rm unused parameters
    
    * assert `this` to be type `any`
    
    to avoid build error `Property 'label' does not exist on type 'CombinedVueInstance<Vue, unknown, unknown, unknown, Readonly<Record<never, any>>>'.`

---
+++ web/pages/resource/_key.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  fetch({ redirect, params }) {
+    const location = `/page/${params.key || ''}`
+    redirect(303, encodeURI(location))
+  }
+})
+</script>
---
+++ web/types/SparqleResponse.d.ts
@@ -0,0 +1,38 @@
+interface RdfTerms1 {
+  type: 'uri'
+  value: string
+}
+
+interface RdfTerms2 {
+  type: 'literal'
+  value: string
+}
+
+interface RdfTerms3 {
+  type: 'literal'
+  value: string
+  'xml:lang': string
+}
+
+interface RdfTerms4 {
+  type: 'literal'
+  value: string
+  datatype: string
+}
+
+interface RdfTerms5 {
+  type: 'bnode'
+  value: string
+}
+
+export interface SparqleResponse {
+  head: {
+    vars: string[]
+    link: string[]
+  }
+  results: {
+    bindings: {
+      [key: string]: RdfTerms1 | RdfTerms2 | RdfTerms3 | RdfTerms4 | RdfTerms5
+    }[]
+  }
+}

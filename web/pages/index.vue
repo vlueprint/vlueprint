6e2b69412145d352b17bcb1297229a3eb6fedc11
takanakahiko <takanakahiko@gmail.com>
2020-04-19 01:21:22 +0900

    トップページをいい感じにした (#41)
    * chore: install sass deps
    
    * feat: add resouces
    
    * feat: create top page
    
    * feat: mobile rayout

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,72 +1,134 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        vlueprint-web
-      </h1>
-      <h2 class="subtitle">
-        My legendary Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <section class="hero">
+      <Logo />
+    </section>
+    <section class="content">
+      <p class="concept">「Virtual Worldに共通のスキーマを」</p>
+      <div class="concept-details">
+        <p>Vlueprint は Virtual 世界の人やモノに関するオープンデータプラットフォームです．</p>
+        <p>様々なデータを共通のスキーマにして管理，提供することが目的です．</p>
       </div>
-    </div>
+    </section>
+    <section class="points content">
+      <div class="point">
+        <p class="point-title">つながりを可視化する</p>
+        <img class="point-img" src="~/assets/hubspot.svg" />
+        <p>RDF 形式で管理した Linked Open Data を取り扱います</p>
+        <p>今までバラバラだった情報を紐付けていくと新たな発見があるかもしれません．</p>
+      </div>
+      <div class="point">
+        <p class="point-title">皆で作る。皆で使う。</p>
+        <b-icon icon="account-group" size="is-large" class="point-img"></b-icon>
+        <p>Vlueprint は GitHub 上で公開され，オープンに運営されています．</p>
+        <p>また，GitHubアカウントを持っていればどなたでも編集や開発に参加することができます，</p>
+      </div>
+    </section>
+    <section class="lets-start content">
+      <h2 class="title">さあ，始めましょう</h2>
+      <div class="links">
+        <a href="#" target="_blank" class="button--green">Document</a>
+        <a href="https://github.com/vlueprint/vlueprint" target="_blank" class="button--grey">GitHub</a>
+      </div>
+    </section>
+    <footer class="footer has-text-centered">
+      <p>© Copyright Vlueprint All rights reserved.</p>
+    </footer>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   components: {
     Logo
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "~bulma/sass/utilities/_all";
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
+  width: 100%;
+}
+
+.content {
+  margin: 80px auto;
+  width: 90%;
+  max-width: 1100px;
+  text-align: center;
+}
+
+.hero {
+  background: #eeeeee;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.concept {
+  margin-top: 50px;
+  text-align: center;
+  font-size: 2rem;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.concept-details {
+  margin-top: 20px;
+  text-align: center;
+  font-size: 1.1rem;
+}
+
+@media only screen and (max-width: 600px) {
+  .concept {
+    font-size: 1.2rem;
+    margin-bottom: 40px;
+    font-weight: bold;
+  }
+  .concept-details {
+    font-size: 1rem;
+  }
+}
+
+.points {
+  display: flex;
+  width: 80%;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  max-width: 800px;
+  margin-top: 30px;
+}
+
+.point {
+  max-width: 350px;
+  text-align: center;
+  margin-top: 60px;
+
+  p {
+    margin-top: 10px;
+  }
+  .point-title {
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+
+  .point-img {
+    width: 48px;
+  }
+}
+
+.lets-start {
+  margin-top: 150px;
 }
 
 .links {
-  padding-top: 15px;
+  margin-top: 20px;
+  padding-top: 20px;
+  text-align: center;
 }
 </style>

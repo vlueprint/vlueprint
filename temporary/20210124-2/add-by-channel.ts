c8e623fe97eadda447cbe6420c3ab86d6ad39fa7
takanakahiko <takanakahiko@gmail.com>
2021-01-24 08:17:11 +0900

    userlocal のデータをチャンネル名から推定して追加する (#88)
    * ad scripts for fetch
    
    * add userlocalID to VirtualBeing's property
    
    * add schema for userlocalID
    
    * add json data
    
    * format rdf files
    
    * add script add-by-channel
    
    * run add-by-channel
    
    * fix conflict
    
    * fix conflict
    
    * format rdfs

---
+++ temporary/20210124-2/add-by-channel.ts
@@ -0,0 +1,74 @@
+import fs from 'fs'
+import * as N3 from 'n3'
+import { DataFactory } from 'n3'
+
+const { namedNode, literal, defaultGraph, quad } = DataFactory;
+
+interface TurtleInfo {
+  quads: N3.Quad[]
+  base: string,
+  prefixes: { [key: string]: string }
+  path: string
+}
+
+const parse = (path: string) => {
+  const text = fs.readFileSync(path, 'utf-8')
+  const parser = new N3.Parser();
+  const quads = parser.parse(text)
+  return {
+    quads: quads,
+    base: (parser as any)._base,
+    prefixes: (parser as any)._prefixes,
+    path: path
+  } as TurtleInfo
+}
+
+const write = async (turtleInfo: TurtleInfo) => {
+  const { quads, base, prefixes, path } = turtleInfo
+
+  const writer = new N3.Writer({ prefixes: prefixes })
+
+  // 書き出すときの書式を弄っているけど複雑なので気にしなくていいです
+  const replaceFunction = (proxied: { apply: Function }) => {
+    (writer as any)._writeQuad = function (subject: { equals: Function }) {
+      if (!subject.equals(this._subject)) {
+        this._write(this._subject === null ? '' : ';\n    .\n\n')
+        this._subject = null
+      }
+      proxied.apply(this, arguments)
+    }
+  }
+  replaceFunction((writer as any)._writeQuad)
+
+  writer.addQuads(quads)
+  const baseReg = new RegExp(base.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g')
+  new Promise((resolve, reject) => {
+    writer.end((error, result) => {
+      if (error) reject()
+      result = `@base <${base}> .\n\n` + result.replace(baseReg, '')
+      result = result.slice(0, -2) + ';\n    .\n';
+      fs.writeFileSync(path, result)
+      resolve(null)
+    })
+  })
+}
+
+const main = async () => {
+  const userLocalData = JSON.parse(fs.readFileSync("nokori.json", 'utf-8')) as {[key: string]: string }
+  const parseret = parse('../../sparql-endpoint/toLoad/resource-VirtualBeing.ttl')
+  let quads = parseret.quads
+  const store = new N3.Store(quads)
+  quads.forEach( q => {
+    const replacedObj = q.object.value.replace("https://vlueprint.org/resource/", "")
+    if( q.predicate.value == "https://vlueprint.org/schema/youtubeChannelName" && userLocalData[replacedObj] ) {
+      const obj = literal(userLocalData[replacedObj])
+      store.addQuad(q.subject, namedNode("https://vlueprint.org/schema/userlocalID"), obj)
+      delete(userLocalData[replacedObj])
+    }
+  })
+  parseret.quads = store.getQuads(null, null, null, null)
+  await write(parseret)
+  fs.writeFileSync("nokori.json", JSON.stringify(userLocalData, null , 2))
+}
+
+main()
